7a630b477272d1a8492a066244a1d8d9671e17e1
mercykorkoressel <mercyessel1005@gmail.com>
2024-02-12 10:50:39 +0000

    work on APPY Nav bar

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var firstname = 'MERCY';
+var lastname = 'ESSEL';
+var age =19;
+
+// let fullname=firstname +' '+ lastname;
+let fullname =`${firstname} ${lastname}`
+// let fullname =`${firstname +' '+ lastname}`
+console.log(fullname);
+
+if(age>=18)
+{console.log('WELCOME TO XXX');
+}
+else{
+     console.log('THIS PAGE IS NOT FOR YOU!!!');
+    }
+
+// for(let i = 1; i !=8 ;i++)
+for (let i = 1; 1++;i !=8 )
+{
+     console.log (' the value if i is',i);
+}
+
+
+// defining function
+function iseligible(){
+ if(age>=18)
+{console.log('WELCOME TO XXX');
+}
+else{
+     console.log('THIS PAGE IS NOT FOR YOU!!!');
+    }
+}
+
+
+console.log("we are using GIT!");
